a9e5c175258e93e68daa5434cb17fe147c238abb
BeryNice <berenice.lemus@gmail.com>
2018-06-14 22:06:11 -0500

    adaptación de funciones a cipher

--- src/cipher.js
+++ src/cipher.js
@@ -5,17 +5,53 @@ offset: 1,
 
 encode: (offset,string) => {
   //return offset + string;
-
+  string = inputString.value;
+   offset = parseInt(inputOffset.value);
+  //codificar caracter por caracter midiendo la longitid del mensaje
+  for (let i = 0; i < string.length; i++) {
+    let message = string.charCodeAt(i);
+    console.log(string.charCodeAt(i));
+    console.log(offset);
+    if (string.charCodeAt(i) === 32 ){
+      console.log("espacio");
+      //espacios
+      messageEncode += String.fromCharCode(32);
+    } else if (message >=97 && message <= 122) {
+      //minusculas
+      messageEncode += String.fromCharCode(((message - 97 + offset) % 26) +97);
+    } else if (message >=65 && message <= 90 ) {
+      //Aplicando la formula mayusculas (x-65+n)%26 +65
+      messageEncode += String.fromCharCode(((message - 65 + offset) % 26) +65);
+    }
+  }
 },
 
 decode: (offset,string) => {
   //return offset -string;
+  string = inputString.value;
+   offset = parseInt(inputOffset.value);
+  //codificar mensaje midiendo su longitid
+  for (let i = 0; i < string.length; i++) {
+    let message = string.charCodeAt(i);
+    console.log(string.charCodeAt(i));
+    console.log(offset);
+    if (string.charCodeAt(i) === 32) {
+      //espacios
+      messageEncode += String.fromCharCode(32);
+    } else if (message >=97 && message <= 122) {
+      //minusculas
+      messageEncode += String.fromCharCode(((message + 97 - offset) % 26) +97);
+    } else if (message >=65 && message <= 90){
+    //Aplicando la formula (x+n)%+26
+    messageEncode += String.fromCharCode(((message + 65 - offset) % 26) +65);
+    }
+  }
 
 },
 
 
 
-//hacker edition
+/*//hacker edition
 createCipherWithOffset: () =>{
   offset:,
   encode:,
@@ -26,4 +62,5 @@ createCipherWithOffset: () =>{
 createCipherWithOffset.emcode();
 
 createCipherWithOffset.decode();
+*/
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-//declarando variables
+//VARIABLES GLOBALES
+//declaramos la variable donde se almacena el mensaje de entrada
+let inputString = document.getElementById('stringMessage');
+//declaramos la variable donde se almacena la posición a desplazar
+let inputOffset = document.getElementById('keychainOffset');
+//reasignando el valor de entrada a nuevas variables
+let string;
+let offset = "";
+// declaramos una nueva variable en donde se almacena el mensaje codificado
+let messageEncode = "";
