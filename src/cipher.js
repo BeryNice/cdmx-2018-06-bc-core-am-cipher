7f548da0c96e65d6edcfa282d11cdcee60e8b455
BeryNice <berenice.lemus@gmail.com>
2018-06-17 13:26:43 -0500

    cipher mejorado, aplicacion de estilos, ajuste de html

--- src/cipher.js
+++ src/cipher.js
@@ -1,58 +1,58 @@
 window.cipher = {
   // ...
 
-offset: 1,
+  offset: 1,
 
-encode: (offset,string) => {
-  //return offset + string;
-  string = inputString.value;
-   offset = parseInt(inputOffset.value);
-  //codificar caracter por caracter midiendo la longitid del mensaje
-  for (let i = 0; i < string.length; i++) {
-    let message = string.charCodeAt(i);
-    console.log(string.charCodeAt(i));
-    console.log(offset);
-    if (string.charCodeAt(i) === 32 ){
-      console.log("espacio");
-      //espacios
-      messageEncode += String.fromCharCode(32);
-    } else if (message >=97 && message <= 122) {
-      //minusculas
-      messageEncode += String.fromCharCode(((message - 97 + offset) % 26) +97);
-    } else if (message >=65 && message <= 90 ) {
-      //Aplicando la formula mayusculas (x-65+n)%26 +65
-      messageEncode += String.fromCharCode(((message - 65 + offset) % 26) +65);
+  encode: (offset,string) => {
+    //return offset + string;
+    string = inputString.value;
+    offset = parseInt(inputOffset.value);
+    //codificar caracter por caracter midiendo la longitid del mensaje
+    for (let i = 0; i < string.length; i++) {
+      let message = string.charCodeAt(i);
+      console.log(string.charCodeAt(i));
+      console.log(offset);
+      if (string.charCodeAt(i) === 32 ){
+        console.log("espacio");
+        //espacios
+        messageEncode += String.fromCharCode(32);
+      } else if (message >=97 && message <= 122) {
+        //minusculas
+        messageEncode += String.fromCharCode(((message - 97 + offset) % 26) +97);
+      } else if (message >=65 && message <= 90 ) {
+        //Aplicando la formula mayusculas (x-65+n)%26 +65
+        messageEncode += String.fromCharCode(((message - 65 + offset) % 26) +65);
+      }
     }
-  }
-},
+    return messageEncode;
+  },
 
-decode: (offset,string) => {
-  //return offset -string;
-  string = inputString.value;
-   offset = parseInt(inputOffset.value);
-  //codificar mensaje midiendo su longitid
-  for (let i = 0; i < string.length; i++) {
-    let message = string.charCodeAt(i);
-    console.log(string.charCodeAt(i));
-    console.log(offset);
-    if (string.charCodeAt(i) === 32) {
-      //espacios
-      messageEncode += String.fromCharCode(32);
-    } else if (message >=97 && message <= 122) {
-      //minusculas
-      messageEncode += String.fromCharCode(((message - 97 - offset) % 26) +97);
-    } else if (message >=65 && message <= 90){
-    //Aplicando la formula (x+n)%+26
-    messageEncode += String.fromCharCode(((message + 65 - offset) % 26) +65);
+  decode: (offset,string) => {
+    //return offset -string;
+    string = inputString.value;
+    offset = parseInt(inputOffset.value);
+    //codificar mensaje midiendo su longitid
+    for (let i = 0; i < string.length; i++) {
+      let message = string.charCodeAt(i);
+      console.log(string.charCodeAt(i));
+      console.log(offset);
+      if (string.charCodeAt(i) === 32) {
+        //espacios
+        messageDecode += String.fromCharCode(32);
+      } else if (message >=97 && message <= 122) {
+        //minusculas
+        messageDecode += String.fromCharCode(((message - 97 - offset) % 26) +97);
+      } else if (message >=65 && message <= 90){
+        //Aplicando la formula (x+n)%+26
+        messageDecode += String.fromCharCode(((message + 65 - offset) % 26) +65);
+      }
     }
-  }
-
-},
-
+    return messageDecode;
+  },
 
 
-/*//hacker edition
-createCipherWithOffset: () =>{
+  /*//hacker edition
+  createCipherWithOffset: () =>{
   offset:,
   encode:,
   decode:,
--- src/index.js
+++ src/index.js
@@ -8,11 +8,42 @@ let string;
 let offset = "";
 // declaramos una nueva variable en donde se almacena el mensaje codificado
 let messageEncode = "";
+let messageDecode = "";
+// declarando botones
+let btnEncode = document.getElementById('buttonEncode');
+let btnDecode = document.getElementById('buttonDecode');
+let btnReset = document.getElementById('buttonReset')
 
 
 // Imprimimos mensaje en HTML
 //  document.getElementById('encryptedMessage').innerHTML = messageEncode;
 
+/*
+document.getElementById("buttonEncode").addEventListener("click", cipher.encode(offset,string){
+    document.getElementById("encryptedMessage").innerHTML = "Hello World";
+});
+*/
 
 
-document.getElementById('encryptedMessage').addEventListener('click',decode(offset,string));
+btnEncode.addEventListener('click', () => {
+  string = inputString.value;
+  offset = parseInt(inputOffset.value);
+  cipher.encode(offset,string);
+  encryptedMessage.innerHTML = messageEncode;
+} );
+
+
+
+btnDecode.addEventListener('click', () => {
+  string = inputString.value;
+  offset = parseInt(inputOffset.value);
+  cipher.decode(offset,string);
+  encryptedMessage.innerHTML = messageDecode;
+} );
+
+//let string = //valor del input anterior
+
+btnReset.addEventListener('click', () => {
+    document.getElementById('coder').reset();
+    document.getElementById('encryptedMessage').innerHTML = "Aquí se mostrará tu mensaje cifrado/descifrado";
+} );
